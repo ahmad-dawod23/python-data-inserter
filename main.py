d1689509d56cd1a51aee8eff5e9c478d6147d20b
Ahmad Dawod <149299234+ahmad-dawod23@users.noreply.github.com>
2024-06-09 15:07:46 +0300

    stuff

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-import requests
+import pyodbc
 import randomname
-import json
-from tokenauth import TokenAuth
+import random
+from multiprocessing import Process
 
-#response = requests.get('https://website.example/id', headers={'Authorization': 'access_token myToken'})
+conn = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=testDB;UID=sa;PWD=ahmad@123;Encrypt=no'
 
-
-url = 'https://localhost:44327/api/Accounts/Login'
-myobj = {
-  "name": "hamodaboi",
-  "email": "no@thanku.com",
-  "password": "hamodaboi"
-}
-x = requests.post(url, json = myobj,verify='D:\stuff\localhost.pem')
-json_response = x.json()
-token=json_response["access_token"]
+cnxn = pyodbc.connect(conn, autocommit=True)
+crsr = cnxn.cursor()
 
 
-
-url2 = 'https://localhost:44327/api/Categories'
-categories= {
-  "name": "food",
-  "imageUrl": "string",
-  "imageArray": "string"
-}
-y = requests.post(url2, json = categories,verify='D:\stuff\localhost.pem', auth=TokenAuth(token))
-json_response2 = y.json()
+def rundbinsertiontask():
+    for x in range(9088878):
+        rand = random.randint(10000, 30000)
+        name = randomname.get_name()
+        # print(name, rand)
+        crsr.execute("exec Sp_AddEmployee ?, Male, ?", name, rand)
 
 
-print(json_response2)
+
+
+
+
+if __name__ == '__main__':
+    p1 = Process(target=rundbinsertiontask)
+    p2 = Process(target=rundbinsertiontask)
+    p3 = Process(target=rundbinsertiontask)
+    p4 = Process(target=rundbinsertiontask)
+    p5 = Process(target=rundbinsertiontask)
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+
+    print("finished")
